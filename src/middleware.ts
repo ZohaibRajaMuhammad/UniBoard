cd01b570ddef39145b88dcaaf0c1f43b44a4d1c4
ZohaibRajaMuhammad <rajamuhammadzohaib00@gmail.com>
2026-05-30 22:39:51 +0500

    Guard Clerk middleware when env is missing

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,12 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import { isClerkServerConfigured } from "@/lib/deployment";
 
-export default clerkMiddleware();
+const guardedClerkMiddleware = isClerkServerConfigured
+  ? clerkMiddleware()
+  : () => NextResponse.next();
+
+export default guardedClerkMiddleware;
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"]
